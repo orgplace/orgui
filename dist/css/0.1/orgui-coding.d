bd8721d1861419ca9992b14ded87ad3fbac444ad
Masatoshi Tsushima <utisam@gmail.com>
2017-06-26 02:53:50 +0900

    Add ignore

--- dist/css/0.1/orgui-coding.d
+++
@@ -1,4 +0,0 @@
-../dist/css/0.1/orgui-coding.css: /home/utisam/workspace/orgplace/ui/orgui/less/lib/namespace.less /home/utisam/workspace/orgplace/ui/orgui/less/lib/colors.less /home/utisam/workspace/orgplace/ui/orgui/less/lib/third-party/pygmentize.css 
-/home/utisam/workspace/orgplace/ui/orgui/less/lib/namespace.less:
-/home/utisam/workspace/orgplace/ui/orgui/less/lib/colors.less:
-/home/utisam/workspace/orgplace/ui/orgui/less/lib/third-party/pygmentize.css:
--- dist/css/demo.d
+++
@@ -1,2 +0,0 @@
-../dist/css/demo.css: /home/utisam/workspace/orgplace/ui/orgui/less/lib/colors.less 
-/home/utisam/workspace/orgplace/ui/orgui/less/lib/colors.less:
