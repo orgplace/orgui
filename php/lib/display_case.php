4a26585caf8f1b782fd6e7ed8c9389870b4a6ab8
Masatoshi Tsushima <utisam@gmail.com>
2018-08-16 01:50:56 +0900

    Add suffix to sample directory

--- php/lib/display_case.php
+++ php/lib/display_case.php
@@ -5,9 +5,9 @@ function displayCase($name)
 
 <div class="displayCase">
   <div class="displayCase__sample">
-    <?php require('sample/' . $name . '.html'); ?>
+    <?php require('sample-html/' . $name . '.html'); ?>
   </div>
-  <?php require('sample/' . $name . '.highlite.html'); ?>
+  <?php require('sample-html/' . $name . '.highlite.html'); ?>
 </div>
 
 <?php
