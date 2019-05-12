8d61a33b0f683bcf0909d1d186587e72a4206bd1
Masatoshi Tsushima <utisam@gmail.com>
2019-05-13 01:23:19 +0900

    Add description to use react

--- php/src/index.php
+++ php/src/index.php
@@ -13,7 +13,7 @@ html5(0, _('Org UI'), function () {
   <header class="ouiFlexSideMargin" style="text-align: center; padding: 40px 0">
       <h2>Org UI</h2>
       <p style="max-width: 800px; margin: 0 auto">
-        Org UI is CSS-centered design Framework.
+        Org UI is CSS-centered design framework.
         Scripts in this framework are reference implementation.
         We allow existing of other scripting ecosystem.
         Therefore, we do not limit the style of your JavaScript!
@@ -21,18 +21,29 @@ html5(0, _('Org UI'), function () {
   </header>
 </div>
 
-<section class="ouiFlexSideMargin" style="padding: 40px 0">
+<section class="ouiFlexSideMargin" style="padding-top: 40px">
   <h3>Getting Started</h3>
 
   <div class="ouiCodeBlock"><pre><span></span><span class="p">&lt;</span><span class="nt">link</span> <span class="na">rel</span><span class="o">=</span><span class="s">&quot;stylesheet&quot;</span> <span class="na">href</span><span class="o">=</span><span class="s">&quot;<script>document.write(location.toString().replace(/index(?:\.html)?$/, ''));</script>css/0.1/orgui.css&quot;</span><span class="p">&gt;</span></pre></div>
 </section>
 
-<section class="ouiFlexSideMargin" style="padding: 40px 0">
+<section class="ouiFlexSideMargin" style="padding-top: 40px">
   <h3>JavaScript</h3>
 
+  <p>
+    In addition to CSS file, use <code class="ouiCode">orgui-js.js</code> to enable JavaScript features.
+    Global object <code class="ouiCode">orgUi</code> is used as a namespace.
+  </p>
   <div class="ouiCodeBlock"><pre><span></span><span class="nt">&lt;script</span> <span class="na">src=</span><span class="s">&quot;<script>document.write(location.toString().replace(/index(?:\.html)?$/, ''));</script>js/0.1/orgui-js.js&quot;</span> <span class="na">defer</span><span class="nt">&gt;&lt;/script&gt;</span></pre></div>
+</section>
 
-  <p>Global object <code class="ouiCode">orgUi</code> is used as a namespace.</p>
+<section class="ouiFlexSideMargin" style="padding-top: 40px">
+  <h3>React</h3>
+
+  <p>
+    If you use React, install <code class="ouiCode">@orgplace/orgui-react</code> and add <code class="ouiCode">orgui.css</code> to your HTML.
+  </p>
+  <div class="ouiCodeBlock"><pre><span></span>npm i @orgplace/orgui-react</pre></div>
 </section>
 
 <?php
